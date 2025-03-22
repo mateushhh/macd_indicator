a120a77112f07cc16ae7b641137a54787d156e5a
Mateusz Grzonka <mateuszgrzonka2003@gmail.com>
2025-03-22 03:33:27 +0100

    Added simulation, with results

--- main.py
+++ main.py
@@ -55,23 +55,63 @@ def get_intersections(macd, signal):
             if (macd[i] > signal[i]):
                 intersections.append([i, macd[i], '^g'])
 
+    if intersections[0][2] == 'vr':
+        intersections.pop(0)
+    if intersections[-1][2] == '^g':
+        intersections.pop(-1)
+
     return intersections
 
 
 def draw_MACD(macd, signal, x0=0, x1=0):
     # Plotting lines
     if x0==0 and x1==0:
-        plt.plot(macd, 'b')
-        plt.plot(signal, 'r')
-    plt.plot(range(x0, x1), macd[x0 : x1], 'b')
-    plt.plot(range(x0, x1), signal[x0 : x1], 'r')
+        plt.plot(macd, 'b', label="MACD")
+        plt.plot(signal, 'r', label="SIGNAL")
+    else:
+        plt.plot(range(x0, x1), macd[x0 : x1], 'b', label="MACD")
+        plt.plot(range(x0, x1), signal[x0 : x1], 'r', label="SIGNAL")
 
     # Plotting transaction points
     intersections = get_intersections(macd, signal)
     for point in intersections:
         if point[0]>=x0 and point[0]<=x1:
             plt.plot(point[0], point[1], point[2], markersize=8)
+    plt.legend(loc='lower right')
     plt.show()
+
+
+def simulate(intersections, close, start_money=1000):
+    pretransaction_money = money = start_money
+    amount_bought = 0
+    transactions = [0,0] # [successful, failed]
+
+    for point in intersections:
+        if(point[0]>=52):
+            # Buying
+            if point[2]=='^g':
+                pretransaction_money = money
+                amount_bought = money / close[point[0]]
+                money = 0
+            # Selling
+            else:
+                money += amount_bought * close[point[0]]
+                amount_bought = 0
+                if(money > pretransaction_money):
+                    transactions[0] +=1
+                else:
+                    transactions[1] +=1
+                print(f"{point[0]}:\t{pretransaction_money:.2f} -> {money:.2f}")
+
+    print(f"\nTotal transactions: {transactions[0] + transactions[1]}")
+    print(f"Profitable transactions: {transactions[0]}")
+    print(f"Failed transactions: {transactions[1]}")
+
+    print(f"\nMoney before transactions: {start_money:.2f}")
+    print(f"Money after transactions: {money:.2f}")
+    print(f"Earnings: {money - start_money:.2f}")
+    print(f"Profit [%]: {money / start_money * 100:.2f}")
+    return money
 
 
 def main():
@@ -81,9 +121,9 @@ def main():
     macd = MACD(close)
     signal = SIGNAL(macd)
     intersections = get_intersections(macd, signal)
+    draw_MACD(macd, signal, 0, 200)
+    simulate(intersections, close)
 
-    draw_MACD(macd, signal, 0,356)
-    plt.show()
-
+    #TODO Wallet graph for showcasing the simulation progress
 
 main()
